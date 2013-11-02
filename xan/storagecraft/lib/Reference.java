18a112ba047dd0de5c7102cdbc138b37852e00de
xandayn <xandayn101@hotmail.com>
2013-11-01 22:51:19 -0700

    Updated the version number to version 1.0 from 0.1

--- xan/storagecraft/lib/Reference.java
+++ xan/storagecraft/lib/Reference.java
@@ -3,7 +3,7 @@ package xan.storagecraft.lib;
 public class Reference {
 	public static final String MOD_ID = "xan_storagecraft";
 	public static final String MOD_NAME = "StorageCraft";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "1.0";
 	public static final String CLIENT_SIDE = "xan.storagecraft.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "xan.storagecraft.proxy.CommonProxy";
 	public static final String CHANNEL = "xanPacketChannel";
